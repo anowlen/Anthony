34f536fd6bd68cae67a2b111bc8a1abfbf62c312
Alanna Nowlen <anowlen@usc.edu>
2025-04-09 06:06:48 +0100

    Laser code that works

--- copy2_24tgt.py
+++ copy2_24tgt.py
@@ -247,7 +247,7 @@ def cartesian_to_servo_angles(x, y, z):
     This means the beam for (1,2,-1) hits the ground twice as far forward as for (1,1,-1).
     """
     # Horizontal servo angle: based on x and y.
-    horizontal_angle = -(math.degrees(math.atan2(y,x)) - 180)
+    horizontal_angle = -(math.degrees(math.atan2(x,y)) - 180) - 180
     
     # Compute the elevation angle E (in degrees) from the horizontal needed
     # to hit the ground, given the mounting height H0 and forward distance y.
@@ -261,7 +261,7 @@ def cartesian_to_servo_angles(x, y, z):
     return horizontal_angle, vertical_angle
 
 #cartesian_coords = (1,1,-1)  # Example coordinates
-target_horizontal_angle, target_vertical_angle = cartesian_to_servo_angles(-312, 457, -150)
+target_horizontal_angle, target_vertical_angle = cartesian_to_servo_angles(83, 340.4, -233.7)
 print("Target Horizontal Angle:", target_horizontal_angle)  # Should print 0°
 print("Target Vertical Angle:", target_vertical_angle) 
 target_vertical_angle = target_vertical_angle + 7
@@ -361,7 +361,7 @@ finally:
     servo_x.stop()
     servo_y.stop()
     print("laser stay on")
-    time.sleep(10) #make laser stay on
+    time.sleep(300) #make laser stay on
     GPIO.output(LASER_PIN, GPIO.LOW)
     GPIO.cleanup()  # Clean up GPIO settings!
 
